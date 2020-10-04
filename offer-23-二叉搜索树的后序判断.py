26825ed5dbc62001bcf16fa3854609b5cdade888
shellmay <51846667+shellmay@users.noreply.github.com>
2020-10-04 15:59:36 +0800

    Update offer-23-二叉搜索树的后序判断.py

--- offer-23-二叉搜索树的后序判断.py
+++ offer-23-二叉搜索树的后序判断.py
@@ -1,23 +1,27 @@
 # -*- coding:utf-8 -*-
+#二叉搜索树特点：结点的左子树都比根小，结点的右子树都比根大，后序遍历则为左右中，最后一个为根节点
+#解题思路：
+#找出第一个比根节点大的数，划分左右子树，由于前面已经判断左子树大小，所以接下来判断右子树是否都比根节点大即可。递归实现整个树的判断。
 class Solution:
     def VerifySquenceOfBST(self, sequence):
-        # write code here
-        if not sequence:return False
-        return self.if_tree(sequence)
-    def if_tree(self,s):
-        if not s:return True
-        root=s[-1]
-        index=0
-        for i in range(len(s)):
-            if s[i]>=root:
-                index=i
+        if sequence is None or len(sequence)==0:
+            return False
+        n=len(sequence)
+        num=sequence[-1]
+        i=0
+        for i in range(n):
+            if sequence[i]>num:
                 break
-        left=s[:index]
-        right=s[index:-1]
-        for i in right:
-            if i<root:
+        for j in range(i,n):
+            if sequence[j]<num:
                 return False
-        return self.if_tree(left) and self.if_tree(right)
+        left=True
+        if i >0:
+            left=self.VerifySquenceOfBST(sequence[:i])
+        right=True
+        if i<n-1:
+            right=self.VerifySquenceOfBST(sequence[i:-1])
+        return left and right
 
 a=[1,6,3,2,5]
 s=Solution()
