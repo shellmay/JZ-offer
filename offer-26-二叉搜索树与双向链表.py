31e20d62b8dede529ca4d1cb7c1a5b987565adf2
shellmay <51846667+shellmay@users.noreply.github.com>
2020-10-04 21:18:20 +0800

    Update offer-26-二叉搜索树与双向链表.py

--- offer-26-二叉搜索树与双向链表.py
+++ offer-26-二叉搜索树与双向链表.py
@@ -11,6 +11,8 @@ class Solution:
         if self.listHead==None:
             self.listHead,self.listTail = pRootOfTree,pRootOfTree
         else:
+            #值得注意的是，上一个结点的右子树指向当前结点，当前结点的左子树指向前一个结点。
+            #因为当前子树的右节点还存在的时候如果改变指向会丢失结点信息
             self.listTail.right,pRootOfTree.left = pRootOfTree,self.listTail
             self.listTail = pRootOfTree
         self.Convert(pRootOfTree.right)
