f9e163637298d83ebb47bd7e0e4f525bbc35e24a
shellmay <51846667+shellmay@users.noreply.github.com>
2020-10-04 21:16:33 +0800

    Update offer-26-二叉搜索树与双向链表.py

--- offer-26-二叉搜索树与双向链表.py
+++ offer-26-二叉搜索树与双向链表.py
@@ -1,3 +1,5 @@
+#由于二叉树的中序遍历为有序的数组，座椅根据中序遍历转换二叉树节点为双向链表
+
 class Solution:
     def __init__(self):
         self.listHead = None
@@ -7,11 +9,9 @@ class Solution:
             return
         self.Convert(pRootOfTree.left)
         if self.listHead==None:
-            self.listHead = pRootOfTree
-            self.listTail = pRootOfTree
+            self.listHead,self.listTail = pRootOfTree,pRootOfTree
         else:
-            self.listTail.right = pRootOfTree
-            pRootOfTree.left = self.listTail
+            self.listTail.right,pRootOfTree.left = pRootOfTree,self.listTail
             self.listTail = pRootOfTree
         self.Convert(pRootOfTree.right)
         return self.listHead
