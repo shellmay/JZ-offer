a617a1a59be1ff96cd37def6d26c229937340756
shellmay <51846667+shellmay@users.noreply.github.com>
2020-10-04 17:31:11 +0800

    Update offer-24-二叉树中为某一值的路径.py

--- offer-24-二叉树中为某一值的路径.py
+++ offer-24-二叉树中为某一值的路径.py
@@ -24,3 +24,24 @@ class Solution:
                 qu.append([head.left,expectNumber-head.left.val,list_node+[head.left.val]])
 
         return res
+    #深度优先的算法
+    #思想：
+    #1、当目标值为0并且为叶子节点走完时，满足条件加入结果中
+    #2、深度遍历左右子树发现该路劲
+    #3、一次递归完毕时不符合条件要pop出结果，即回溯
+    #4、注意深浅拷贝：cur[:]和cur的区别
+    def FindPath(self, root, expectNumber):
+        res, cur = [], []
+        def DFS(root, expectNumber):
+            if not root:
+                return
+            cur.append(root.val)
+            expectNumber -= root.val
+            if expectNumber == 0 and root.left == None and root.right == None:
+                res.append(cur[:])
+            DFS(root.left, expectNumber)
+            DFS(root.right, expectNumber)
+            cur.pop()
+
+        DFS(root,expectNumber)
+        return res
