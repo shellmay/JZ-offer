6d98285a06b860882c680afffd19111c6b49ee29
shellmay <51846667+shellmay@users.noreply.github.com>
2020-10-05 13:03:18 +0800

    Update offer-62-二叉树第K小的数.py

--- offer-62-二叉树第K小的数.py
+++ offer-62-二叉树第K小的数.py
@@ -1,3 +1,4 @@
+#全部遍历
 class Solution:
     def KthNode(self, pRoot, k):
         res = []
@@ -18,6 +19,21 @@ class Solution:
         for i in rsnode:
             if i.val==res[k-1]:
                 return i
+#中序遍历
+class Solution:
+    count = 0
+    def KthNode(self, pRoot, k):
+        if not pRoot:
+            return None
+        node = self.KthNode(pRoot.left, k)
+        if node:
+            return node
+        self.count += 1
+        if self.count == k:
+            return pRoot
+        node = self.KthNode(pRoot.right, k)
+        if node:
+            return node
 
 
 
