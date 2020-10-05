55129b6eb516944b2ae0fe301e4465444376759c
shellmay <51846667+shellmay@users.noreply.github.com>
2020-10-05 12:21:51 +0800

    Update offer-60-把二叉树打印成多行.py

--- offer-60-把二叉树打印成多行.py
+++ offer-60-把二叉树打印成多行.py
@@ -25,3 +25,30 @@ class Solution:
             curLayerNodes = nextLayerNodes
             resultArray.append(sorted(curLayerValues))
         return resultArray
+    
+    # -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#用栈存储一层的结点
+import collections
+class Solution:
+    # 返回二维列表[[1,2],[4,5]]
+    def Print(self, pRoot):
+        if not pRoot:return []
+        re=[pRoot]
+        result=[]
+        while re:
+            c=[]
+            rec=[]
+            for cur in re:
+                c.append(cur.val)
+                if cur.left:
+                    rec.append(cur.left)
+                if cur.right:
+                    rec.append(cur.right)
+            result.append(c)
+            re = rec
+        return result
