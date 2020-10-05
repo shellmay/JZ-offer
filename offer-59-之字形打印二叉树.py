bc4516aad14e2e291c022f6b7dd17ec73c699f45
shellmay <51846667+shellmay@users.noreply.github.com>
2020-10-05 11:00:08 +0800

    Update offer-59-之字形打印二叉树.py

--- offer-59-之字形打印二叉树.py
+++ offer-59-之字形打印二叉树.py
@@ -4,22 +4,35 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+#1、利用两个栈，先进后出的特点做这道题。
+#2、一个栈顺着存，一个栈逆着存
+#
+#
 class Solution:
     def Print(self, pRoot):
-        # write code here
-        if not pRoot:
-            return []
-        index=True
-        end=[pRoot]
-        en=[]
-        while pRoot:
-            tmp=[]
-            index=not index
-            for i in end:
-                tmp.append(i.val)
-                if i.left:
-                    end.append(i.left)
-                if i.right:
-                    end.append(i.right)
-            en.append(tmp[::-1] ) if index else en.append(tmp)
-        return
+        if not pRoot:return []
+        stack1=[pRoot]
+        stack2=[]
+        rsc=[]
+        while (stack1 or stack2):
+            r1=[]
+            r2=[]
+            while stack1:
+                cur=stack1.pop()
+                r1.append(cur.val)
+                if cur.left:
+                    stack2.append(cur.left)
+                if cur.right:
+                    stack2.append(cur.right)
+            if len(r1)!=0:
+                rsc.append(r1)
+            while stack2:
+                cur=stack2.pop()
+                r2.append(cur.val)
+                if cur.right:
+                    stack1.append(cur.right)
+                if cur.left:
+                    stack1.append(cur.left)
+            if len(r2)!=0:
+                rsc.append(r2)
+        return rsc
