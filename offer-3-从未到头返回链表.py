9236fbe0d4fba09b956d5e6187ffda9b79071b95
shellmay <51846667+shellmay@users.noreply.github.com>
2020-10-06 22:23:07 +0800

    Update offer-3-从未到头返回链表.py

--- offer-3-从未到头返回链表.py
+++ offer-3-从未到头返回链表.py
@@ -3,17 +3,10 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
-
+##递归 回溯
 class Solution:
     # 返回从尾部到头部的列表值序列，例如[1,2,3]
-    def printListFromTailToHead(self, listNode):
-        # write code here
-        l=[]
-        while listNode:
-            l.append(listNode.val)
-            listNode=listNode.next
-        l.reverse()
-        return l
+    return self.printListFromTailToHead(listNode.next)+[listNode.val] if listNode else []
 
 n1=ListNode(1)
 n2=ListNode(2)
