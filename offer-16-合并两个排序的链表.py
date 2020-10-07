51c7ff804e7becc9fdaccde6ea598f7e09993990
shellmay <51846667+shellmay@users.noreply.github.com>
2020-10-07 17:00:57 +0800

    Update offer-16-合并两个排序的链表.py

--- offer-16-合并两个排序的链表.py
+++ offer-16-合并两个排序的链表.py
@@ -1,18 +1,35 @@
 # -*- coding:utf-8 -*-
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class ListNode:
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+#直接循环和递归
 class Solution:
-    def HasSubtree(self, pRoot1, pRoot2):
-        # write code here
-        if not pRoot1 or not pRoot2:
-            return False
-        return self.is_butree(pRoot1,pRoot2) or self.HasSubtree(pRoot1.left,pRoot2) or self.HasSubtree(pRoot1.right,pRoot2)
-    def is_butree(self,a,b):
-        if not b:
-            return True
-        if not a or a.val!=b.val:
-            return False
-        return self.is_butree(a.left,b.left) and self.is_butree(a.right,b.right)
+    def Merge(self, pHead1, pHead2):
+        #伪造一个头结点
+        merge=cur=ListNode(0)
+        #当都存在时，小的作为下一个结点
+        while pHead1 and pHead2:
+            if pHead1.val<pHead2.val:
+                cur.next=pHead1
+                pHead1=pHead1.next
+            else:
+                cur.next=pHead2
+                pHead2=pHead2.next
+            cur=cur.next
+        #当有一个链表为空时，下一个就是剩下的链表
+        cur.next=pHead1 if pHead1 else pHead2
+        #注意返回头结点是伪头结点的下一个结点
+        return merge.next
+    
+    
+    
+    #递归时判断小的值，递归回溯时从后往前接入链表
+    def Merge(self, pHead1, pHead2):
+        if not pHead1:return pHead2
+        if not pHead2:return pHead1
+        if pHead1.val<pHead2.val:
+            pHead1.next=self.Merge(pHead1.next,pHead2)
+            return pHead1
+        pHead2.next=self.Merge(pHead1,pHead2.next)
+        return pHead2
