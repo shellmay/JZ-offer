883f5d3aa69a1b070046d12e41e6781db9a37c7e
shellmay <51846667+shellmay@users.noreply.github.com>
2020-10-07 14:25:57 +0800

    Update offer-14-链表中倒数的第K个节点.py

--- offer-14-链表中倒数的第K个节点.py
+++ offer-14-链表中倒数的第K个节点.py
@@ -3,19 +3,17 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
+#两个指针距离k的单位，当前面的指针到达最后的一个结点时，说明前一个结点已经到达倒数第k个结点
 class Solution:
     # 返回ListNode
-    def ReverseList(self, pHead):
-        # write code here
-        last=None
-        cur=pHead
-        if pHead==None:
-            return pHead
-        else:
-            while cur!=None:
-                temp = cur.next
-                cur.next = last
-                last = cur
-                cur = temp
-
-            return last
+    def FindKthToTail(self,head,k):
+        #前后指针
+        formar,latter=head,head
+        #前指针先走k步
+        for i in range(k):
+            if formar:formar=formar.next
+            else:return
+        #同时走，直到前指针走完链表
+        while formar:
+            formar,latter=formar.next,latter.next
+        return latter
